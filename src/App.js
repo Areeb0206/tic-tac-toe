74241fbc3099fd254a822104ed9e8df0c1a7b07d
Areeb0206 <masafvi48@gmail.com>
2021-03-25 18:14:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Tictactoe from './Tictactoe/Tictactoe'
+
+function App() {
+  return (
+    <div>
+    
+     <Tictactoe />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Tictactoe/Tictactoe.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import './Tictactoe.css';
+
+class Tictactoe extends Component{
+
+	constructor(props){
+		super(props)
+		this.state = {
+
+             game:Array(9).fill(null),
+             player:"x"
+
+		}
+
+	}
+
+winner(){
+	let wincases=[
+
+		["0","1","2"],
+		["3","4","5"],
+		["6","7","8"],
+		["0","3","6"],
+		["1","4","7"],
+		["2","5","8"],
+		["0","4","8"],
+		["2","4","6"]
+	]
+
+
+	for(let i=0;i<wincases.length;i++){
+
+		const [a,b,c]=wincases[i];
+
+		if(this.state.game[a] && this.state.game[a] === this.state.game[b] && this.state.game[a]===this.state.game[c]){
+			alert("you won");
+		}
+	}
+
+}
+
+
+ handleClick(index){
+
+    let newgame=this.state.game
+
+    if( newgame[index] === null ){
+    	  newgame[index]=this.state.player
+
+    let newplayer=this.state.player === "x" ? "o" : "x"
+
+    this.setState({
+       game:newgame,
+       player:newplayer
+    })
+
+    }
+      
+  
+    this.winner()
+
+ 	console.log(this.state.game)
+
+ }
+
+render()
+{
+const Box = this.state.game.map
+
+        ((box,index) => 
+
+    	<div className="box" key={index} onClick={() =>
+
+    	 this.handleClick(index)}>
+
+    	 <p>{box}</p></div>)
+
+  return (
+    <div>
+    
+         <div className="tic-tac-toe">
+
+
+            <div className="heading">tic-tac-toe</div>
+
+
+
+            <div className="game-box">
+
+
+
+               {Box}
+                
+
+            </div>
+
+                
+
+
+
+
+         </div>
+
+
+
+
+    </div>
+  );
+ }
+}
+
+export default Tictactoe;
+
+
